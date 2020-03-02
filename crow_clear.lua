e18ad2d3d54e0cb4d14b31734c044898591118dc
nattog <gpurssell@gmail.com>
2020-03-02 23:28:25 +0000

    tidy up

--- crow_clear.lua
+++
@@ -1,8 +0,0 @@
---- crow clear
-
-function init()
-  for n = 1, 2 do
-    input[n].mode("none")
-    input[n].mode("none")
-  end
-end
--- gate_probs.lua
+++ gate_probs.lua
@@ -3,18 +3,17 @@
 -- out1-4: 100% 75% 50% 25%
 
 function init()
-  input[1].mode("change")
-  input[2].mode("none")
-  for n = 1, 4 do
-    output[n].slew = 0.001
-  end
+    input[1].mode("change")
+    input[2].mode("none")
+    for n = 1, 4 do
+        output[n].slew = 0.001
+    end
 end
 
 input[1].change = function(s)
-  print("s")
-  for n = 1, 4 do
-    if math.random() * 100 <= 100 / n then
-      output[n](pulse(0.01, 8))
+    for n = 1, 4 do
+        if math.random() * 100 <= 100 / n then
+            output[n](pulse(0.01, 8))
+        end
     end
-  end
 end
