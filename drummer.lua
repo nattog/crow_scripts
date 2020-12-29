1e42e50e5e4d71ced387159371481342c20cfdb6
nattog <gpurssell@gmail.com>
2020-12-29 19:14:43 +0000

    update drummer

--- drummer.lua
+++ drummer.lua
@@ -1,17 +1,26 @@
 -- drummer
--- uses sequence notation like this 'x---x---x---x---'
--- by setting command seq[output_no] = 'x--'
+-- 4 track pattern sequencer
+-- pattern notation looks like this 'x---x---x---x---'
+-- update sequence track with set(track, pattern)
+-- 
+-- queue a new sequence
+-- progress(seq table, period - by default 16)
+--
+-- control transport
+-- start() stop() reset()
 
-local counter = 0 -- used to get index by modulo of seq items
-seq = {
-    "", "", "", ""
-}
+counter = 0 -- used to get index by modulo of seq items
+seq = {"", "", "", ""}
+pause = false
+preset = {'x---', '----x---', '--x-', 'xx--x--'}
+queued = {}
+wait = 0
 
 function init()
     -- set up input modes
     input[1].mode("change", 1, 0.1, "rising")
     input[2].mode("change", 1, 0.1, "rising")
-    input[1].change = step
+    input[1].change = function() if pause == false then step() end end
     input[2].change = reset
 
     -- set up output modes
@@ -27,19 +36,32 @@ end
 
 -- update counter and trigger for each 'x' found at index of each seq
 function step()
-    counter = counter + 1
+    local to_print = ""
     for chan = 1, #seq do
         if chan <= 4 then
             if #seq[chan] > 0 then
                 local step_idx = math.floor(counter % #seq[chan]) + 1
-                local step_value = seq[chan]:sub(step_idx, 1)
+                local step_value = seq[chan]:sub(step_idx, step_idx)
                 if step_value == 'x' then
-                   print('triggers -> ' .. chan)
-                   output[chan]()
+                    output[chan]()
                 end
+                to_print = to_print .. (step_value == 'x' and step_value or "_") .. " "
+            else 
+                to_print = to_print .. "_ "
             end
         end
     end
+    counter = counter + 1
+    if #queued > 0 then
+        if wait > 0 then
+            wait = wait - 1
+        else 
+            seq = queued
+            wait = 0
+            queued = {}
+        end
+    end
+    print(to_print)
 end
 
 function set(idx, val)
@@ -51,5 +73,38 @@ function set(idx, val)
         
     if idx < 5 and idx > 0 then
         seq[idx] = #val > 0 and val or ''
+    end
+
+    print('\n')
+    print('SET ' .. idx .. ' TO ' .. string.upper(seq[idx]))
+    print('\n')
+end
+
+function stop()
+    pause = true
+end
+
+function start()
+    pause = false
+end
+
+function progress(tab, period)
+    local progress_on = period or 16
+    if tab == nil then
+        print('no input')
+        return
+    end
+    local new_seq = #tab < 1 and {"", "", "", ""} or tab
+    if pause == false then
+        wait_period = progress_on % counter 
+        if wait_period ~= 0 then
+            queued = new_seq
+            wait = wait_period
+        else
+            seq = new_seq
+        end
+    else 
+        seq = new_seq
+        reset()
     end
 end
